1ca4d5ec762bcf463c33ff8aecb0e3319098b55e
Sam-Marsden-Wolfhound <164749740+Sam-Marsden-Wolfhound@users.noreply.github.com>
2024-09-11 16:53:43 +0100

    clean up

--- visualizations.py
+++ visualizations.py
@@ -49,7 +49,7 @@ def display_graph(title, dataframe, default_columns=[]):
 
 
 def display_graph_plotly(title, dataframe, default_columns=[]):
-    with st.container():
+    with st.container(border=True):
         st.subheader(title)
         if not dataframe.empty:
             df = dataframe.copy()
@@ -57,38 +57,68 @@ def display_graph_plotly(title, dataframe, default_columns=[]):
             min_index = 0
             max_index = len(df) - 1
 
-            # Create sliders to select the start and end times
-            start = st.slider(
-                'Select start time',
-                min_value=min_index,
-                max_value=max_index,
-                value=min_index,
-                key=f'{title}_min'
-            )
-            end = st.slider(
-                'Select end time',
-                min_value=min_index,
-                max_value=max_index,
-                value=min(max_index, 300),  # Limit default to 300 if max_index is larger
-                key=f'{title}_max'
-            )
-
-            # Ensure that the end time is after the start time
-            if start > end:
-                st.error('End time must be after start time')
-                return
-
-            # Filter the DataFrame based on the selected start and end times
-            df_filtered = df.iloc[start:end+1]
-
             # Let the user select columns to plot
             columns_to_plot = st.multiselect(
                 "Select columns to plot",
-                df_filtered.columns.tolist(),
+                df.columns.tolist(),
                 default=default_columns
             )
 
             if columns_to_plot:
+                with st.expander("X & Y Axis", expanded=False):
+                    st.subheader("X-axis", divider=False)
+                    # Create sliders to select the start and end times
+                    start = st.slider(
+                        'Select start time',
+                        min_value=min_index,
+                        max_value=max_index,
+                        value=min_index,
+                        key=f'{title}_min'
+                    )
+                    end = st.slider(
+                        'Select end time',
+                        min_value=min_index,
+                        max_value=max_index,
+                        value=min(max_index, 300),  # Limit default to 300 if max_index is larger
+                        key=f'{title}_max'
+                    )
+
+                    # Ensure that the end time is after the start time
+                    if start > end:
+                        st.error('End time must be after start time')
+                        return
+
+                    # Filter the DataFrame based on the selected start and end times
+                    df_filtered = df.iloc[start:end + 1]
+
+                    st.subheader("Y-axis", divider=False)
+                    # Get the min and max values in the selected columns to define y-axis limits
+                    min_value = df_filtered[columns_to_plot].min().min()
+                    max_value = df_filtered[columns_to_plot].max().max()
+
+                    # Add sliders to allow users to limit the y-axis range
+                    y_min = st.slider(
+                        "Select lower bound of y-axis",
+                        min_value=float(min_value),
+                        max_value=float(max_value),
+                        value=float(min_value),
+                        step=(max_value - min_value) / 100,
+                        key=f'{title}_y_min'
+                    )
+
+                    y_max = st.slider(
+                        "Select upper bound of y-axis",
+                        min_value=float(min_value),
+                        max_value=float(max_value),
+                        value=float(max_value),
+                        step=(max_value - min_value) / 100,
+                        key=f'{title}_y_max'
+                    )
+
+                    if y_min >= y_max:
+                        st.error('Upper bound must be greater than lower bound')
+                        return
+
                 # Use Plotly to create the graph with a specified x-axis column
                 fig = px.line(
                     df_filtered,
@@ -96,6 +126,9 @@ def display_graph_plotly(title, dataframe, default_columns=[]):
                     y=columns_to_plot,
                     labels={'x': 'Year-Month'}
                 )
+
+                # Set y-axis limits
+                fig.update_yaxes(range=[y_min, y_max])
 
                 # Reduce the number of x-axis labels displayed by controlling the number of ticks (nticks)
                 fig.update_xaxes(
